ee60368367c61cc55459a9573a5e0f65c4300b7e
201815069 <70687333+eunchae01@users.noreply.github.com>
2022-04-10 21:49:44 +0900

    ui구현

---
+++ todolist/src/components/TodoCreate.js
@@ -0,0 +1,105 @@
+import React, {useState} from 'react';
+import styled, {css} from 'styled-components';
+//더하기모양 아이콘
+import {MdAdd} from 'react-icons/md';   
+
+const CircleButton = styled.button`
+    background: #38d9a9;
+
+    &:hover{
+        background: #63e6be;
+    }
+
+    &:active{
+        background: #20c997;
+    }
+
+    z-index: 5; //다른 것 가려야하기 때문
+    cursor: pointer;
+    width: 80px;
+    height: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    //가운데 맨 아래 보여지게 됨
+    position: absolute;
+    left: 50%;
+    bottom: 0px;
+    //버튼의 위치 더 정확하게 설정(해당 버튼사이즈의 오십퍼씩 더 움직임)
+    transform: translate(-50%, 50%);
+
+    font-size: 60px;    //아이콘 크기
+    color: white;
+    border-radius: 40px;
+
+    border: none;
+    outline: none;
+
+    transition: 0.125s all ease-in;
+
+    //오픈이라는 값이 투르일때 css
+    ${props => props.open && css`
+        background: #ff6b6b;
+
+        &:hover{
+            background: #ff8787;
+        }
+
+        &:active{
+            background: #fa5252;
+        }
+
+        transform: translate(-50%, 50%) rotate(45deg);
+    `}
+`;
+
+// 어떤 폼의 위치를 정해주는 것
+const InsertFormPositioner = styled.div`
+    width: 100%;
+    bottom: 0;
+    left: 0;
+    position: absolute;
+`;
+
+const InsertForm = styled.div`
+    background: #f8f9fa;
+    padding: 32px;
+    padding-bottom: 72px;
+    //모서리 둥글게했던거 안삐져나오게 하는것
+    border-bottom-left-radius: 16px;
+    border-bottom-right-radius: 16px;
+    border-top: 1px solid #e9ecef;
+`;
+
+const Input = styled.input`
+    padding: 12px;
+    border-radius: 4px;
+    border: 1px solid #dee2e6;
+    width: 100%;
+    outline: none;
+    font-size: 18px;
+    box-sizing: border-box;
+`;
+
+function TodoCreate(){
+    const [open, setOpen] = useState(false);
+    const onToggle = () => setOpen(!open);
+
+    return(
+        <>
+        {open && (
+            <InsertFormPositioner>
+                <InsertForm>
+                    <Input placeholder='할 일을 입력 후, Enter를 눌러주세요' autoFocus/>
+                </InsertForm>
+            </InsertFormPositioner>
+        )}
+        <CircleButton onClick={onToggle} open={open}>
+            <MdAdd/>
+        </CircleButton>
+        </>
+    );
+}
+
+export default TodoCreate;
---
+++ todolist/src/components/TodoItem.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import styled, {css} from 'styled-components';
+import {MdDone, MdDelete} from 'react-icons/md';
+
+// 왼쪽에 있는 체크를 보여주는 컴포넌트
+const CheckCircle = styled.div`
+    width: 32px;
+    height: 32px;
+    border-radius: 16px;
+    border: 1px solid #ced4da;
+    font-size: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+    cursor: pointer;
+
+    ${props => props.done && css`
+        border: 1px solid #38d9a9;
+        color: #38d9a9;
+    `}
+`;
+
+// 텍스트 보여주는 컴포넌트
+const Text = styled.div`
+    flex: 1;
+    font-size: 21px;
+    color: #495057;
+
+    ${props => props.done && css`
+        color : #ced4da;
+    `}
+`;
+
+// 쓰레기통 아이콘 보여주는 컴포넌트
+const Remove = styled.div`
+    opacity: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #dee2e6;
+    font-size: 24px;
+    cursor: pointer;
+    &:hover{
+        color: #ff6b6b;
+    }
+`;
+
+// 위 세개의 내용이 이 안에 들어옴
+const TodoItemBlock = styled.div`
+    display: flex;
+    align-items: center;
+    padding-top: 12px;
+    padding-bottom: 12px;
+
+    &:hover{
+        ${Remove}{
+            opacity: 1;
+        }
+    }
+`;
+
+function TodoItem({id, done, text}){
+    return(
+        <TodoItemBlock>
+            <CheckCircle done={done}>{done && <MdDone/>}</CheckCircle>
+            <Text done={done}>{text}</Text>
+            <Remove>
+                <MdDelete/>
+            </Remove>
+        </TodoItemBlock>
+    );
+}
+
+export default TodoItem;
